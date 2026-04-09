9d6cbe1fcfe85ea6cb8712be37edc57c0bca7590
Pisit Kongnarong <pisit@ppknr.com>
2026-04-09 09:55:21 +0700

    feat: add service worker for caching and define base styles for money exchange app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qp086vj9vc8"
+    "revision": "0.ld89bc5f7h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
