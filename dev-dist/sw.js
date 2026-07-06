b74f733b3d1e0aee4dd67e3b4fe4ab7a9255dfdd
Pisit <pisit@ppknr.com>
2026-07-06 17:12:59 +0700

    Update service worker cache revision
    Update the cache revision hash in dev-dist/sw.js for index.html. This is typically generated automatically when the index.html file is rebuilt to ensure the service worker cache is properly invalidated.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mv4f1lp73h8"
+    "revision": "0.kuguviskvjc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
